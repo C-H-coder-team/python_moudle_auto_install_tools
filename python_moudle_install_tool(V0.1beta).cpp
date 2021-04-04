023237ccdd4cbf97a257ee59c9941e02195a2ade
hacker-Fanjun <78690857+hacker-Fanjun@users.noreply.github.com>
2021-04-04 16:59:42 +0800

    Add files via upload

---
+++ python_moudle_install_tool(V0.1beta).cpp
@@ -0,0 +1,95 @@
+#include<windows.h>
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<time.h>
+//头文件
+using namespace std;
+//命名空间
+int main()
+{
+	cout << "注意事项：" << endl
+		<< "1.有python" << endl
+		<< "2.安装时打开了pip选项" << endl
+		<< "3.网络良好(ping python.org 的avg.>=300)" << endl
+		<< "4.是Windows系统" << endl
+		<< "全部满足？是<Y>/否<任意键>" << endl;
+	char i1;
+	cin >> i1;
+	if (i1 == 'Y' || i1 == 'y')
+	{
+		Sleep(500);
+		system("pip install pygame");
+		Sleep(500);
+		system("pip install pysocket");
+		Sleep(500);
+		system("pip install pillow");
+		Sleep(500);
+		system("pip install tornado");
+		Sleep(500);
+		system("pip install mysql-connector");
+		Sleep(500);
+		system("pip install pymongo");
+		Sleep(500);
+		system("pip install SQLAlchemy");
+		Sleep(500);
+		system("pip install mongoengine");
+		Sleep(500);
+		system("pip install django");
+		Sleep(500);
+		system("pip install flask");
+		Sleep(500);
+		system("pip install cp");
+		Sleep(500);
+		system("pip install girlfriend");
+		Sleep(500);
+		system("pip install boyfriend");
+		Sleep(500);
+		system("pip install pygal");
+		Sleep(500);
+		system("pip install matplotlib");
+		Sleep(500);
+		system("pip install robot");
+		Sleep(500);
+		system("pip install pyos");
+		Sleep(500);
+		system("pip install cpython");
+		Sleep(500);
+		system("pip install pyftp");
+		Sleep(500);
+		system("pip install line");
+		Sleep(500);
+		system("pip install pycode");
+		Sleep(500);
+		system("pip install math");
+		Sleep(500);
+		system("pip install pycom");
+		Sleep(500);
+		system("pip install download");
+		Sleep(500);
+		system("pip install port");
+		Sleep(500);
+		system("pip install language");
+		Sleep(500);
+		system("pip install geometry");
+		Sleep(500);
+		system("pip install pyapp");
+		Sleep(500);
+		system("pip install pycode");
+		Sleep(500);
+		system("pip install pyhttp");
+		Sleep(500);
+		system("pip install pyline");
+		Sleep(500);
+		system("pip install upload");
+		Sleep(500);
+		cout << endl<<"install over";
+	}
+	else return 0;
+	return 0;
+	/*
+	time:GMT+8 2021/04/04 16:51 
+	coder:hacker_fanjun
+	Follow GPL open source agreement
+	*/
+}
