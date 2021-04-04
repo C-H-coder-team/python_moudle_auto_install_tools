e274bfd80b3cda954404516ef52fcc841185b10e
hacker-Fanjun <78690857+hacker-Fanjun@users.noreply.github.com>
2021-04-04 19:16:52 +0800

    Add files via upload

---
+++ python moudle install by powershell(v0.1 alpha).ps1
@@ -0,0 +1,72 @@
+﻿#
+# Script.ps1
+#
+pip install --upgrade setuptools	
+
+pip install pygame
+		
+pip install pysocket
+	
+pip install pillow
+		
+pip install tornado
+		
+pip install mysql-connector
+		
+pip install pymongo
+		
+pip install SQLAlchemy
+		
+pip install mongoengine
+		
+pip install django
+		
+pip install flask
+		
+pip install cp
+		
+pip install girlfriend
+		
+pip install boyfriend
+		
+pip install pygal
+	
+pip install matplotlib
+		
+pip install robot
+		
+pip install pyos
+		
+pip install cpython
+		
+pip install pyftp
+		
+pip install line
+		
+pip install pycode
+		
+pip install math
+		
+pip install pycom
+		
+pip install download
+		
+pip install port
+		
+pip install language
+		
+pip install geometry
+		
+pip install pyapp
+		
+pip install pycode
+		
+pip install pyhttp
+		
+pip install pyline
+		
+pip install upload
+
+pip install pyinstaller
+		
+#time：2021/04/04 17:13
